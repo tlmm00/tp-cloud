c3346a2a9790aea58df6fc0175aed4ee2a70725b
tlmm00 <tluigimm@gmail.com>
2024-11-25 12:13:42 -0300

    first commit

---
+++ client.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+
+int main() {
+
+    char *ip = "127.0.0.1";
+    int port = 5566;
+
+    int sock;
+
+    struct sockaddr_in addr;
+    socklen_t addr_size;
+
+    char buffer[1024];
+    int n;
+
+    // creating socket
+    sock = socket(AF_INET, SOCK_STREAM, 0);
+    if (sock < 0){
+        perror("[-] Socket error");
+        exit(1);
+    }
+    printf("[+] TCP server socket created.\n");
+
+    memset(&addr, '\0', sizeof(addr));
+    addr.sin_family = AF_INET;
+    addr.sin_port = port;
+    addr.sin_addr.s_addr = inet_addr(ip);
+
+    // connect to server
+    connect(sock, (struct sockaddr*)&addr, sizeof(addr));
+    printf("Connected to the server.\n");
+
+    return 0;
+}
---
+++ server.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+
+int main() {
+
+    char *ip = "127.0.0.1";
+    int port = 5566;
+
+    int server_sock, clinet_sock;
+
+    struct sockaddr_in server_addr, client_addr;
+    socklen_t addr_size;
+
+    char buffer[1024];
+    int n;
+
+    // creating server socket
+    server_sock = socket(AF_INET, SOCK_STREAM, 0);
+    if (server_sock < 0){
+        perror("[-] Socket error");
+        exit(1);
+    }
+    printf("[+] TCP server socket created.\n");
+
+    memset(&server_addr, '\0', sizeof(server_addr));
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = port;
+    server_addr.sin_addr.s_addr = inet_addr(ip);
+
+    // bind the address and port number
+    n = bind(server_sock, (struct sockaddr*)&server_addr, sizeof(server_addr));
+    if (n<0){
+        perror("[-] Bind error");
+        exit(1);
+    }
+    printf("[+] Bind to the port number: %d\n", port);
+
+    // listen to the client
+    listen(server_sock, 5);
+    printf("Listening...\n");
+
+    while(1){
+        addr_size = sizeof(client_addr);
+        clinet_sock = accept(server_sock, (struct sockaddr*)&client_addr, &addr_size);
+        printf("[+] Client connected.\n");
+    }
+
+
+    return 0;
+}
